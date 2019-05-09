a251652f36a4fe36b2ff4d4566b73c0dcdb8769e
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-09 09:32:28 +0700

    adding tabs on graph

--- src/sections/GrafikPengamatan.js
+++ src/sections/GrafikPengamatan.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
-import GrafikDoughnut from "../components/GrafikDoughnut";
-import GrafikBar from "../components/GrafikBar";
+import React, { Component } from 'react';
+import GrafikDoughnut from '../components/GrafikDoughnut';
+import GrafikBar from '../components/GrafikBar';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-let dataArea = require("../data/dataArea.json");
-let dataTabelSimulasi = require("../data/dataTabelSimulasi.json");
+let dataArea = require('../data/dataArea.json');
+let dataTabelSimulasi = require('../data/dataTabelSimulasi.json');
 
 export default class GrafikPengamatan extends Component {
   render() {
@@ -23,20 +25,23 @@ export default class GrafikPengamatan extends Component {
     return (
       <div id="section3" className="container">
         <h1 align="center">Grafik Pengamatan</h1>
-        <br />
-        <br />
-        <GrafikDoughnut
-          judul={"Grafik Frekuensi Tujuan Antar"}
-          labelArea={labelArea}
-          dataArea={dataAreaProps}
-        />
-        <br />
-        <GrafikBar
-          judul={"Grafik Total Waktu Pelayanan"}
-          labelGraph={"Total Waktu Pelayanan dalam Menit"}
-          labelArea={labelTabel}
-          dataArea={dataTabel}
-        />
+        <Tabs defaultIndex={0} onSelect={index => console.log(index)}>
+          <TabList>
+            <Tab>Grafik Frekuensi Tujuan Antar</Tab>
+            <Tab>Grafik Total Waktu Pelayanan</Tab>
+          </TabList>
+          <TabPanel>
+            <GrafikDoughnut judul={'Grafik Frekuensi Tujuan Antar'} labelArea={labelArea} dataArea={dataAreaProps} />
+          </TabPanel>
+          <TabPanel>
+            <GrafikBar
+              judul={'Grafik Total Waktu Pelayanan'}
+              labelGraph={'Total Waktu Pelayanan dalam Menit'}
+              labelArea={labelTabel}
+              dataArea={dataTabel}
+            />
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
